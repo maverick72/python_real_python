56996490bbacdf26b82e89e4419e11b4d55a723a
Eric Berthiaume <eric.berthiaume@gmail.com>
2020-05-12 14:06:43 +0200

    Initial

---
+++ sum.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+
+input1 = input("Enter a value for a: ")
+input2 = input("Enter a value for b: ")
+
+sum = add(int(input1), int(input2))
+print(f"{input1} + {input2} = {sum}")
